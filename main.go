b891edc3f106ca4360390125752d0a57487146bf
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-25 09:15:05 -0500

    moved to function

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "os/exec"
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"runtime"
+)
 
 func main() {
-	url := "https:fenwickelliott.io"
 
-	exec.Command("open", url).Start()
+	err := openBrowser("https:fenwickelliott.io")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func openBrowser(url string) error {
+	switch runtime.GOOS {
+	case "darwin":
+		exec.Command("open", url).Start()
+	default:
+		return errors.New("Unrecogniszed os: " + runtime.GOOS)
+	}
+	return nil
 }
